f2ddec652c4f85aed2466ef36e7328df7361dbef
Hermer29 <kliman.chan@gmail.com>
2020-12-09 17:20:03 +0300

    Model account implementation [1/2]

--- blog/app/Http/Controllers/AccountController.php
+++ blog/app/Http/Controllers/AccountController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Accounts;
 
 class AccountController extends Controller
 {
     public function execute(Request $data)
     {
-        dd($data);
+        $input_login = $data -> input('login');
+        $input_password = $data -> input('password');
+        $select_stmnt = "SELECT password = $input_password AND login = $input_login FROM blog.accounts WHERE login = $input_login AND password = $input_password";
+        
+        dd(Accounts::select($select_stmnt));
+        
     }
 }
