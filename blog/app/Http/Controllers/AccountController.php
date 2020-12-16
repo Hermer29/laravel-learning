acab1727d1b06987f07dfdd6015038641fc3e03d
Hermer29 <kliman.chan@gmail.com>
2020-12-16 11:10:37 +0300

    UNK

--- blog/app/Http/Controllers/AccountController.php
+++ blog/app/Http/Controllers/AccountController.php
@@ -4,16 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Accounts;
+use App\Http\Requests\Login;
 
 class AccountController extends Controller
 {
-    public function execute(Request $data)
+    public function execute(Login $data)
     {
-        $input_login = $data -> input('login');
-        $input_password = $data -> input('password');
-        $select_stmnt = "SELECT password = $input_password AND login = $input_login FROM blog.accounts WHERE login = $input_login AND password = $input_password";
-        
-        dd(Accounts::select($select_stmnt));
-        
+        echo $data -> input('login');
     }
 }
--- blog/app/Models/Accounts.php
+++ blog/app/Models/Accounts.php
@@ -7,6 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Accounts extends Model
 {
-    protected $table = 'accounts';
     use HasFactory;
 }
